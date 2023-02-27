20baf8e7c1350201fa51714b2b879aeed206b438
Mark Ubiadas <mubiadas@gmail.com>
2023-02-27 12:44:34 -0800

    pagination

--- client/src/pages/all-properties.tsx
+++ client/src/pages/all-properties.tsx
@@ -113,6 +113,7 @@ const AllProperties = () => {
             defaultValue={10}
             onChange={() => { }}
           >
+            {/* pagination */}
             {[10, 20, 30, 40, 50].map((size)=> (
               <MenuItem key={size} value={size}>Show {size}</MenuItem>
             ))}
