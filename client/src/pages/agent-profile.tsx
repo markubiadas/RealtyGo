af89823d757e9d3334f8f5560fb988c59987b97b
Mark Ubiadas <mubiadas@gmail.com>
2023-03-03 14:36:14 -0800

    dashboard edit
    added all properties on dashboard

--- client/src/pages/agent-profile.tsx
+++ client/src/pages/agent-profile.tsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import { useOne } from '@pankod/refine-core'
+import { useParams } from '@pankod/refine-react-router-v6'
+import { Profile } from 'components'
 
-const agentProfile = () => {
+const AgentProfile = () => {
+  const {id} = useParams();
+
+  const{data, isLoading, isError} = useOne({
+    resource:'users',
+    id: id as string
+  })
+
+  console.log(data)
+
+  const MyProfile = data?.data ?? [];
+
+  if(isLoading) return <div>Loading...</div>
+  if(isError) return <div>Error</div>
+
   return (
-    <div>agent-profile</div>
+    <Profile 
+      type="Agent"
+      name={MyProfile.name}
+      email={MyProfile.email}
+      avatar={MyProfile.avatar}
+      properties={MyProfile.allProperties}
+    />
   )
 }
 
-export default agentProfile
+export default AgentProfile
